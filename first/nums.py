fe5cd13582311478111fdf0898fbb068ba4a472c
Arsi44 <arseka22332445@mail.ru>
2020-10-22 18:50:31 +0300

    Добавляем strings.py и изменяем  nums.py

--- first/nums.py
+++ first/nums.py
@@ -1,9 +1,16 @@
 def plus(a, c):
     return a + c
 
+
 def minus(a, c):
     return a - c
+
+
+def multi(a, c):
+    return a * c
+
 
 if __name__ == '__main__':
     print(plus(2, 3))
     print(minus(2, 3))
+    print(multi(2, 3))
